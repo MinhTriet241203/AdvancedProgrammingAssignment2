9e0bd89f40dba02c547ad5840e5a579fd9c47e36
MinhTriet241203 <minhtriet241203@gmail.com>
2022-12-16 20:57:31 +0700

    1

---
+++ View/BookForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace AdvancedProgrammingAssignment2.View
+{
+    public partial class BookForm : Form
+    {
+        public BookForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
