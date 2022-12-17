4583614f3d4ff9d9cabcd79df35f8b033bc09a01
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-12-17 10:55:14 +0700

    login

--- View/Login.cs
+++ View/Login.cs
@@ -5,10 +5,14 @@ namespace AdvancedProgrammingAssignment2.View
 {
     public partial class login : Form
     {
+        //Fields
+        
+        private readonly Panel panelRegister;
         public login()
         {
             InitializeComponent();
         }
+        
 
 
 
@@ -27,6 +31,48 @@ namespace AdvancedProgrammingAssignment2.View
         }
 
         private void RegisterHandle(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+            InitializeComponent().pnRegister.TabIndex = 9;
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
         {
             throw new System.NotImplementedException();
         }
